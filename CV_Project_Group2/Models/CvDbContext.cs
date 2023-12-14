2987c0954ffaeabd5372b51a530e196c1b7b48dc
FilippaEm <filippa.emberg@hotmail.se>
2023-12-14 23:33:59 +0100

    Databasanslutning

---
+++ CV_Project_Group2/Models/CvDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CV_Project_Group2.Models
+{
+    public class CvDbContext : DbContext
+    {
+
+        public CvDbContext(DbContextOptions<CvDbContext>options) : base(options) 
+        { 
+        }
+
+        public  DbSet<Project> Project { get; set; }
+        public  DbSet<User_Project> User_Project { get; set; }
+        public  DbSet<User> User { get; set; }
+        public  DbSet<CV> CV { get; set; }
+
+    }
+}
