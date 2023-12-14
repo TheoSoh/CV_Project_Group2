c2b415792454a27e7702004f797986a0f282393f
Theo <theo.sohlman@gmail.com>
2023-12-14 11:18:31 +0100

    initial commit n' push

---
+++ CV_Project_Group2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CV_Project_Group2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
