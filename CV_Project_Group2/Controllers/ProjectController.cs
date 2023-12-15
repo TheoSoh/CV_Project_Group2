e713c1084558fe86cf5e195fe6b7fb8603a2d047
filippahelin <121015432+filippahelin@users.noreply.github.com>
2023-12-15 12:43:21 +0100

    nya controllers

--- CV_Project_Group2/Controllers/ProjectController.cs
+++ CV_Project_Group2/Controllers/ProjectController.cs
@@ -4,15 +4,9 @@ using CV_Project_Group2.Models;
 
 namespace CV_Project_Group2.Controllers;
 
-    public class UserController : Controller
+
+    public class ProjectController : Controller
     {
-
-        private readonly ILogger<UserController> _logger;
-
-        public UserController(ILogger<UserController> logger)
-        {
-            _logger = logger;
-        }
         public IActionResult Index()
         {
             return View();
