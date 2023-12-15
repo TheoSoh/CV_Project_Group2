c36bf925f8062a9a8c89c1946fbc4d39311bcba5
filippabostrom <120644557+filippabostrom@users.noreply.github.com>
2023-12-15 12:41:09 +0100

    Fixat propertys och nycklar i Models

---
+++ CV_Project_Group2/Models/Education.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CV_Project_Group2.Models
+{
+    public class Education
+    {
+        public int id {  get; set; }
+        public string Education_name { get; set; }
+
+        public int Cv_id { get; set; }
+        [ForeignKey(nameof(Cv_id))]
+        public virtual CV cv { get; set; }
+
+    }
+}
--- CV_Project_Group2/Models/User_Project.cs
+++ CV_Project_Group2/Models/User_Project.cs
@@ -1,6 +1,17 @@
-﻿namespace CV_Project_Group2.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CV_Project_Group2.Models
 {
     public class User_Project
     {
+        public int User_id { get; set; }
+
+        public int Project_id { get; set; }
+
+        [ForeignKey(nameof(User_id))]
+        public User user { get; set; }
+
+        [ForeignKey(nameof(Project_id))]
+        public Project project { get; set; }    
     }
 }
