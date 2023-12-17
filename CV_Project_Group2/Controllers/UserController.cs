568e38b919e0660bddc1a96d1ef9402678200a0e
filippahelin <121015432+filippahelin@users.noreply.github.com>
2023-12-17 17:16:06 +0100

    login och register vyer

--- CV_Project_Group2/Controllers/UserController.cs
+++ CV_Project_Group2/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CV_Project_Group2.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace CV_Project_Group2.Controllers;
 
@@ -8,14 +9,54 @@ namespace CV_Project_Group2.Controllers;
     {
 
         private readonly ILogger<UserController> _logger;
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
 
-        public UserController(ILogger<UserController> logger)
+        public UserController(ILogger<UserController> logger, UserManager<User> userMngr, SignInManager<User> signInMngr)
         {
             _logger = logger;
+            _userManager = userMngr;
+            _signInManager = signInMngr;
         }
-        public IActionResult Index()
+
+        [HttpGet] 
+        public IActionResult Login()
+        {
+            LoginViewModel loginViewModel = new LoginViewModel();
+            return View(loginViewModel);
+
+        }
+
+        [HttpGet]
+
+        public IActionResult Register()
         {
             return View();
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel) {
+
+            if (ModelState.IsValid) {
+
+                User user = new User();
+                user.UserName = registerViewModel.UserName;
+
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+
+                if (result.Succeeded)
+                {
+
+                    await _signInManager.SignInAsync(user, isPersistent: true); // skapar en cookie som tillåter användaren att vara inloggad i flera sessioner samtidigt, även om webbläsaren stängs ner
+                    return RedirectToAction("Index", "HomeController");
+
+                }
+            }
+
+            return View(registerViewModel);
+        }
     }
+
+
 
